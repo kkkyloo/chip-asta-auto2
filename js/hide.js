a1e3fb9141326e416054170ec8c9ab2198f6df2d
kkkyloo <92991764+kkkyloo@users.noreply.github.com>
2023-11-28 21:13:03 +0300

    Add files via upload

--- js/hide.js
+++ js/hide.js
@@ -6,10 +6,10 @@ function toggleText() {
 
     var computedStyle = window.getComputedStyle(moreText);
 
-    if (computedStyle.display === "none" && mobNavbar.display === "none" ) {
+    if (computedStyle.display === "none" && mobNavbar.display === "none") {
         moreText.style.display = "inline";
         btnText.innerHTML = "Скрыть";
-        imgElement.style.height = '70vh';
+        imgElement.style.height = '78.3vh';
     } 
     else if (computedStyle.display === "inline" && mobNavbar.display === "none" ) {
         moreText.style.display = "none";
@@ -25,3 +25,33 @@ function toggleText() {
         btnText.innerHTML = "Подробнее";
     }
 }
+
+function toggleText2() {
+    var moreText = document.getElementById("moreText2");
+    var btnText = document.getElementById("toggleBtn2");
+    var imgElement = document.querySelector('.img_none2');
+    var mobNavbar = window.getComputedStyle(navmob);
+
+    var computedStyle = window.getComputedStyle(moreText);
+
+    if (computedStyle.display === "none" && mobNavbar.display === "none") {
+        moreText.style.display = "inline";
+        btnText.innerHTML = "Скрыть";
+        imgElement.style.height = '62vh';
+    } 
+    else if (computedStyle.display === "inline" && mobNavbar.display === "none" ) {
+        moreText.style.display = "none";
+        btnText.innerHTML = "Подробнее";
+        imgElement.style.height = '36vh';
+    }
+    else if (computedStyle.display === "none" && mobNavbar.display === "block" ) {
+        moreText.style.display = "inline";
+        btnText.innerHTML = "Скрыть";
+    }
+    else{
+        moreText.style.display = "none";
+        btnText.innerHTML = "Подробнее";
+    }
+}
+
+
